d570ba607e626f70c90a13e8862027cf813209ba
Kyle Lin <kylejlin.us@gmail.com>
2017-07-02 17:25:51 -0700

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,27 @@
   ctx.fillStyle = 'black';
   ctx.fillRect(0, 0, window.screen.availWidth, window.screen.availHeight);
   
+  function TouchHandler(canvas) {
+    this.canvas = canvas;
+    
+    this.canvas.addEventListener('touchstart', function(e) {
+      this.onTouchStart(e);
+    });
+    this.canvas.addEventListener('touchmove', function(e) {
+      this.onTouchMove(e);
+    });
+    this.canvas.addEventListener('touchend', function(e) {
+      this.onTouchEnd(e);
+    });
+    
+    this.tapBoxes = [];
+    this.touches = [];
+  }
+  
+  TouchHandler.prototype.onTouchStart = function(e) {
+    
+  };
+  
   window.addEventListener('scroll', function(e) {
     e.preventDefault();
   });
